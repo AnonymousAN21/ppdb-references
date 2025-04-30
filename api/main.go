a29a18a0318a9ce8a24d465b3a6b2c4cb089e840
ROG <reeve.tangel@gmail.com>
2025-04-30 23:19:59 +0800

    sec commit

--- api/main.go
+++ api/main.go
@@ -1,23 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"spmb/back-end/api/internal/controller"
 	"spmb/back-end/api/internal/middleware"
 	"spmb/back-end/api/internal/service"
-	"spmb/back-end/config"
 	"spmb/back-end/config/connection"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
 )
 
 // var jwtSecret = []byte("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDQ0NzM4NDksImlhdCI6MTc0NDQzNzg0OSwiaXNfc3R1ZGVudCI6ZmFsc2UsIm5iZiI6MTc0NDQzNzg0OSwib3JnYW5pemF0aW9uX2lkIjoiIiwicm9sZV9pZCI6MCwidXNlcl9pZCI6Ijk4MThmMzMyLTczMDctNDYxNC05NzcxLTNlODQ5NzY5YmYzOCJ9.h1O3rN73fCK5CAk0wAEek6GBFQBoSQ21hqXhNmWgCnk")
 
 func main() {
-	jwtgen()
 
 	db := connection.Conn()
 	FaqService := service.NewFaqService(db)
@@ -66,22 +60,4 @@ func main() {
 	v1.POST("/accomplishment/calculation/:type", controller.Calc)
 
 	server.Run(":8083")
-}
-
-func jwtgen() {
-	userID := uuid.MustParse("3fd97d6e-430b-4d65-9fc6-6a8d83d0f2cb")
-
-	claims := jwt.MapClaims{
-		"user_id": userID.String(),
-		"exp":     time.Now().Add(time.Hour * 72).Unix(),
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	signed, err := token.SignedString([]byte(config.LoadConfig().JwtToken))
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("JWT Token:")
-	fmt.Println(signed)
 }
